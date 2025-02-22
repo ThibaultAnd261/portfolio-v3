95cf88872d71a30f17445b02a67c159bd202ba87
ThibaultAnd261 <thibaultandwork@gmail.com>
2025-02-22 12:09:05 +0100

    💄 Add Buttons to categorize skills data

---
+++ src/data/skills.js
@@ -0,0 +1,142 @@
+export const skills = [
+    {
+        name: "TypeScript",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
+        alt: "TypeScript",
+        width: 24,
+        height: 24
+    },
+    {
+        name: "PHP",
+        icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/2560px-PHP-logo.svg.png",
+        alt: "PHP",
+        width: 42,
+        height: 24
+    },
+    {
+        name: "C++",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg",
+        alt: "C++",
+        width: 24,
+        height: 24
+    },
+    {
+        name: "C#",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg",
+        alt: "C#",
+        width: 24,
+        height: 24
+    },
+    {
+        name: "Java",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg",
+        alt: "Java",
+        width: 24,
+        height: 24
+    },
+    {
+        name: "Python",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
+        alt: "Python",
+        width: 24,
+        height: 24
+    },
+    {
+        name: "MySQL",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg",
+        alt: "MySQL",
+        width: 24,
+        height: 24
+    },
+    {
+        name: "React",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+        alt: "React",
+        width: 24,
+        height: 24
+    },
+    {
+        name: "Tailwind",
+        icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/1024px-Tailwind_CSS_Logo.svg.png",
+        alt: "Tailwind",
+        width: 30,
+        height: 24
+    },
+    {
+        name: "Laravel",
+        icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Laravel.svg/1200px-Laravel.svg.png",
+        alt: "Laravel",
+        width: 24,
+        height: 24
+    },
+    {
+        name: "AdonisJS",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/adonisjs/adonisjs-original.svg",
+        alt: "AdonisJS",
+        width: 24,
+        height: 24
+    },
+    {
+        name: "Linux",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg",
+        alt: "Linux",
+        width: 24,
+        height: 24
+    },
+    {
+        name: "Unity",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/unity/unity-original.svg",
+        alt: "Unity",
+        width: 24,
+        height: 24
+    },
+    {
+        name: "Figma",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg",
+        alt: "Figma",
+        width: 24,
+        height: 24
+    },
+    {
+        name: "Postman",
+        icon: "https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg",
+        alt: "Postman",
+        width: 24,
+        height: 24
+    },
+    {
+        name: "API",
+        icon: "https://cdn-icons-png.flaticon.com/512/10169/10169724.png",
+        alt: "API",
+        width: 24,
+        height: 24
+    },
+    {
+        name: "Make",
+        icon: "https://cdn.prod.website-files.com/602b981692c0c610c4ea335f/6797654be34f7f6b8c0b0135_Logo%20Make.png",
+        alt: "Make",
+        width: 24,
+        height: 24
+    },
+    {
+        name: "Notion",
+        icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Notion-logo.svg/2048px-Notion-logo.svg.png",
+        alt: "Notion",
+        width: 24,
+        height: 24
+    },
+    {
+        name: "Draw.io",
+        icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Diagrams.net_Logo.svg/2048px-Diagrams.net_Logo.svg.png",
+        alt: "Draw.io",
+        width: 24,
+        height: 24
+    },
+    {
+        name: "Git",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
+        alt: "Git",
+        width: 24,
+        height: 24
+    }
+]
